f9fc8d8d534f57b82637e49d676ffd7a07693ec6
suraj_chaubey <suraj.chaubey@hcltech.com>
2025-10-19 18:34:13 +0530

    completed CFSIR project

--- CFSIRModel/src/commons/model.py
+++ CFSIRModel/src/commons/model.py
@@ -2,43 +2,67 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CFSIRBlock(nn.Module):
-    def __init__(self, input_channels=3, mid_channels=16, output_channels=32, kernel_size=3, stride=1, padding=1):
-        super(CFSIRBlock, self).__init__()
-        self.conv1 = nn.Conv2d(input_channels, mid_channels, kernel_size=kernel_size, padding=padding, stride=stride)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(mid_channels, output_channels, kernel_size=kernel_size, padding=padding, stride=stride)
+class CFSIRHEADBlock(nn.Module):
+    def __init__(self, input_channels=3, output_channels=32, kernel_size=3, stride=1, padding=1):
+        super(CFSIRHEADBlock, self).__init__()
+        self.conv1 = nn.Conv2d(input_channels, output_channels, kernel_size=kernel_size, padding=padding, stride=stride)
+        self.batch_norm = nn.BatchNorm2d(output_channels)
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.relu(self.batch_norm(self.conv1(x)))
+        return x
+    
+class CFSIRMidBlock(nn.Module):
+    def __init__(self,input_channels = 3 , output_channels = 32,max_pool = 2,kernel_size = 3 ,stride = 1,padding = 1):
+        super(CFSIRMidBlock,self).__init__()
+        self.conv1 = nn.Conv2d(input_channels, output_channels, kernel_size=kernel_size, padding=padding, stride=stride)
+        self.batch_norm = nn.BatchNorm2d(output_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.max_pool = nn.MaxPool2d(max_pool)
+
+    def forward(self, x):
+        x = self.max_pool(self.relu(self.batch_norm(self.conv1(x))))
+        return x
+
+class ClassifierBlock(nn.Module):
+    def __init__(self,first_linear_channel_inp,first_linear_channel_out,dropout,num_classes):
+        super(ClassifierBlock,self).__init__()
+        self.adpt_pool = nn.AdaptiveAvgPool2d((1,1))
+        self.first_linear_layer = nn.Linear(first_linear_channel_inp,first_linear_channel_out)
+        self.second_linear_layer = nn.Linear(first_linear_channel_out,num_classes)
+        self.dropout = nn.Dropout(dropout)
+        self.flatten = nn.Flatten()
+
+    def forward(self,x):
+        x = self.adpt_pool(x)
+        x = self.flatten(x)
+        x = F.relu(self.first_linear_layer(x))
+        x = self.dropout(x)
+        x = self.second_linear_layer(x)
+
         return x
 
 class CFSIRModel(nn.Module):
     def __init__(self, num_classes=10):
         super(CFSIRModel, self).__init__()
 
-        # Block 1: 3 -> 16 -> 32
-        self.block1 = CFSIRBlock(input_channels=3, mid_channels=16, output_channels=32)
+        self.head_block1 = CFSIRHEADBlock(input_channels=3,output_channels=64,kernel_size=3,padding=1)
+        self.mid_block1 = CFSIRMidBlock(input_channels=64,output_channels=64,max_pool=2,kernel_size=3,padding=1)
 
-        # Block 2: 32 -> 64 -> 128
-        self.block2 = CFSIRBlock(input_channels=32, mid_channels=64, output_channels=128)
+        self.head_block2 = CFSIRHEADBlock(input_channels=64,output_channels=128,kernel_size=3,padding=1)
+        self.mid_block2 = CFSIRMidBlock(input_channels=128,output_channels=128,max_pool=2,kernel_size=3,padding=1)
+        self.mid_block3 = CFSIRMidBlock(input_channels=128,output_channels=256,max_pool=2,kernel_size=3,padding=1)
 
-        # Additional conv layers: 128 -> 64 -> 32
-        self.conv1 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
-
-        # Fully connected layers
-        self.fc1 = nn.Linear(32 * 1 * 1, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+        self.classifier_block = ClassifierBlock(first_linear_channel_inp=256,first_linear_channel_out=128,dropout=0.5,num_classes=num_classes)
 
     def forward(self, x):
-        x = self.block1(x)      
-        x = self.block2(x)       
-        x = self.pool(F.relu(self.conv1(x)))
-        x = F.relu(self.conv2(x))
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.head_block1(x)
+        x = self.mid_block1(x)
+
+        x = self.head_block2(x)
+        x = self.mid_block2(x)
+        x = self.mid_block3(x)
+
+        x = self.classifier_block(x)
         return x
--- CFSIRModel/src/commons/trainingpipeline.py
+++ CFSIRModel/src/commons/trainingpipeline.py
@@ -1,6 +1,7 @@
 from src.datapreprocessing.dataloader import CFSIRDataLoader
 from src.commons.model import CFSIRModel
 from src.constant.constant import Constants
+from src.utility.tensorboard import TensorBoard
 import torchvision.transforms as transforms
 from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
@@ -20,29 +21,78 @@ class TrainingPipeline:
         self.batch_size = batch_size
         self.model = CFSIRModel().to(self.device)
         self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+        self.optimizer = optim.SGD(self.model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4)
+        self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+        self.optimizer, mode='min', factor=0.1, patience=5
+        )
         self.train_data,self.test_data = CFSIRDataLoader().load_data(Constants.DATA_DIR_PATH,batch_size=self.batch_size)
+        self.writer = TensorBoard.get_tensorboard_writer()
+        dummy_input = torch.randn(1, 3, 32, 32).to(device)
+        self.writer.add_graph(self.model, dummy_input)
 
     def train(self):
-        # outer loop for epcohs 
+        best_val_loss = float('inf')
         for epoch in range(self.epochs):
+            # Training phase
+            self.model.train()
             running_loss = Constants.ZERO
+            correct = 0
+            total = 0
             progress_bar = tqdm(self.train_data, desc=f"Epoch [{epoch+1}/{self.epochs}]", leave=False)
 
-            for images,labels in progress_bar:
+            for images, labels in progress_bar:
                 images, labels = images.to(self.device), labels.to(self.device)
-
-                self.optimizer.zero_grad() 
+                
+                self.optimizer.zero_grad()
                 model_pred = self.model(images)
-                loss = self.criterion(model_pred,labels)
+                loss = self.criterion(model_pred, labels)
                 loss.backward()
                 self.optimizer.step()
 
                 running_loss += loss.item()
+                _, predicted = torch.max(model_pred.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
                 progress_bar.set_postfix(loss=loss.item())
-                
-            print(f"Epoch [{epoch+1}/{self.epochs}] Loss: {running_loss/len(self.train_data):.4f}")
 
+            train_loss = running_loss / len(self.train_data)
+            train_acc = 100 * correct / total
+
+            # Log training metrics
+            self.writer.add_scalar('Loss/train', train_loss, epoch)
+            self.writer.add_scalar('Accuracy/train', train_acc, epoch)
+            self.writer.add_scalar('Learning_rate', self.optimizer.param_groups[0]['lr'], epoch)
+
+            # Validation phase
+            self.model.eval()
+            val_loss = Constants.ZERO
+            correct = 0
+            total = 0
+            with torch.no_grad():
+                for images, labels in self.test_data:
+                    images, labels = images.to(self.device), labels.to(self.device)
+                    outputs = self.model(images)
+                    loss = self.criterion(outputs, labels)
+                    val_loss += loss.item()
+
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+
+            val_loss = val_loss / len(self.test_data)
+            print(f"Epoch [{epoch+1}/{self.epochs}] Training Loss: {train_loss:.4f} | Validation Loss: {val_loss:.4f}")
+
+            # Log validation metrics
+            val_acc = 100 * correct / total
+            self.writer.add_scalar('Loss/validation', val_loss, epoch)
+            self.writer.add_scalar('Accuracy/validation', val_acc, epoch)
+
+            print(f'Epoch [{epoch+1}/{self.epochs}] Train Loss: {train_loss:.4f} Val Loss: {val_loss:.4f} '
+                  f'Train Acc: {train_acc:.2f}% Val Acc: {val_acc:.2f}%')
+
+            # Step the scheduler based on validation loss
+            self.scheduler.step(val_loss)
+            
     def eval(self):
         correct,total = Constants.ZERO , Constants.ZERO
         with torch.no_grad():
@@ -69,11 +119,11 @@ class TrainingPipeline:
             outputs = self.model(img_tensor)
             _, predicted = torch.max(outputs, Constants.ONE)
             pred_class = class_names[predicted.item()]
-
+            confidence = torch.softmax(outputs, dim=1)[0][predicted.item()]
         draw_img = image.copy()
         draw = ImageDraw.Draw(draw_img)
 
-        draw.text((10, 10), f"Predicted: {pred_class}", fill=(255, 0, 0))
+        draw.text((10, 10), f"Predicted: {pred_class}  || Confidence: {confidence.item():.2f}", fill=(255, 0, 0))
 
         plt.imshow(draw_img)
         plt.axis("off")
@@ -91,5 +141,8 @@ class TrainingPipeline:
         self.model.eval()
         print(f"Model weights loaded from {path}")
         return self.model
+    
+    def __del__(self):
+        self.writer.close()
 
 
